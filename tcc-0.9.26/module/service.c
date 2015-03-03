6753b15066455566225dc3baab180ff98ee67bf4
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-03-03 13:56:39 -0600

    started making tcc module service

---
+++ tcc-0.9.26/module/service.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// 
+
+// wrong_usage:
+// prints off the correct usage and quits the program
+void wrong_usage()
+{
+  printf("Usage: tcc_kernel_run flags parameter\n");
+}
+
+int main()
+{
+    
+}
